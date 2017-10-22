0abeafe28ce851d757edd85b1b87977b6655be4b
Jawann <jawann@gmail.com>
2017-10-22 01:40:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(game)
   inside = "-" * 11
-  puts " #{game[0]} | #{game[1]} | #{game[2]}"
+  puts " #{game[0]} | #{game[1]} | #{game[2]} "
   puts inside
-  puts " #{game[3]} | #{game[4]} | #{game[5]}"
+  puts " #{game[3]} | #{game[4]} | #{game[5]} "
   puts inside
-  puts " #{game[6]} | #{game[7]} | #{game[8]}"
+  puts " #{game[6]} | #{game[7]} | #{game[8]} "
 end
